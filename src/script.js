0199ce713e447162764e3813637cea23243d5eaa
Ashutoshm17121999 <ashutoshmishra2022jan@cedcoss.com>
2022-02-08 13:32:35 +0530

    main

--- src/script.js
+++ src/script.js
@@ -1,26 +1,40 @@
-var product=[];
-function call(){
-var id = document.getElementById('pId').value;
-var name = document.getElementById('pName').value;
-var price = document.getElementById('pPrice').value;
-return{
-    'pId': id,
-    'pName': name,
-    'pPrice': price,
+// changes are mentioned below
+var array=[];
+function myFunction(){
+    var id = document.getElementById('pID').value;
+    var name1 = document.getElementById('pName').value;
+    var price = document.getElementById('pPrice').value;
+    myLogic(id, name1, price);
+    myDisplay(array);
 }
-}
-function addProduct(){
-    var products = call()
-    product.push(products);
-    console.log([0]);
-    table = document.getElementById('table1');
-    var row = table.insertRow(-1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    for(var i = 0; i<product.length; i++){
-        cell1.innerHTML = product[i].pId
-        cell2.innerHTML = product[i].pName
-        cell3.innerHTML = product[i].pPrice
+function myLogic(id , name1 , price){
+    for(var i = 0;i<array.length;i++){
+        console.log(array[i]);
+        if (array[i].pID == id)
+            {
+                     alert("Product ID already exists");
+                 return;
+            }
     }
+    obj= {};
+    obj['pID'] = id;
+    obj['pName'] = name1;
+    obj['pPrice'] = price;
+    array.push(obj);
+}
+function myDisplay(result){
+var text = document.getElementById('table1');
+text.innerHTML = "<tr>\
+     <td>Product Id</td>\
+     <td>Product Name</td>\
+     <td>Product Price</td>\
+    </tr>";
+    for(let i =0; i<result.length;i++){
+        text.innerHTML+='<tr>\
+         <td>' + result[i].pID + '</td>\
+         <td>' + result[i].pName + '</td>\
+         <td>' + result[i].pPrice + '</td>\
+     </tr>';
+    }
+    
 }
